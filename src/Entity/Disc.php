c5609a9bb2336df067fbb6895b97370b2dea0e2b
houbron <benjamin.houbrongmail.com>
2024-09-25 11:26:36 +0200

    ils ont casser l'api

--- src/Entity/Disc.php
+++ src/Entity/Disc.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DiscRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: DiscRepository::class)]
 class Disc
 {
